3c3d7add88d1104ae5b43512d3fed9d34ef80558
zelenin.m <irbissssss@net.com>
2025-12-01 07:40:54 +0300

    Fix emotion message

--- Dou-Di-Zhu-Server/src/Websocket/Handlers/emotionUse.Handler.ts
+++ Dou-Di-Zhu-Server/src/Websocket/Handlers/emotionUse.Handler.ts
@@ -23,8 +23,6 @@ export const handleEmotionUse = async(
         Type: MessageType.EMOTION_USE,
         Data: {
             Emotion: Data.Emotion,
-            TargetPositionX: Data.TargetPositionX,
-            TargetPositionY: Data.TargetPositionY,
             UseName: Data.UseName,
             TargetName: Data.TargetName,
         },
@@ -37,8 +35,6 @@ export const handleEmotionUse = async(
         Type: MessageType.EMOTION_USE,
         Data: {
             Emotion: Data.Emotion,
-            TargetPositionX: Data.TargetPositionX,
-            TargetPositionY: Data.TargetPositionY,
             UseName: Data.UseName,
             TargetName: Data.TargetName,
         },
